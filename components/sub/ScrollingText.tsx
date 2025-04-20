60bfb90d153e168171da2ad36f26a438309a4be3
iamasistiwari <iamashishtiwari2004@gmail.com>
2025-04-20 13:42:52 +0530

    more-projects-added

--- components/sub/ScrollingText.tsx
+++ components/sub/ScrollingText.tsx
@@ -8,80 +8,89 @@ import { FaDocker } from "react-icons/fa";
 import { SiExpress } from "react-icons/si";
 import { FaFigma } from "react-icons/fa6";
 import { SiPostgresql } from "react-icons/si";
-import { FaReact } from "react-icons/fa";
+import { SiRedis } from "react-icons/si";
 import { FaAws } from "react-icons/fa";
-import { SiRecoil } from "react-icons/si";
 import { FaJava } from "react-icons/fa";
 import { FaCloudflare } from "react-icons/fa";
+import { FaPython } from "react-icons/fa";
+import { SiMongodb } from "react-icons/si";
 
 
 export default function ScrollingText() {
-  return (
-    <div className='w-full justify-center items-center flex text-neutral-300 rounded-2xl'>
-      <div className='w-full rounded-2xl'>
-        <Marquee autoFill speed={30}>
+    return (
+        <div className='w-full justify-center items-center flex text-neutral-300 rounded-2xl'>
+            <div className='w-full rounded-2xl'>
+                <Marquee autoFill speed={30}>
 
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <FaJava />
-                <p>Java</p>
-            </div>
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <FaJava />
+                        <p>Java</p>
+                    </div>
 
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <FaReact />
-                <p>React</p>
-            </div>
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <RiNextjsFill />
-                <p>Nextjs</p>
-            </div>
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <FaPython />
+                        <p>Python</p>
+                    </div>
 
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <SiTypescript />
-                <p>Typescript</p>
-            </div>
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <RiNextjsFill />
+                        <p>Nextjs</p>
+                    </div>
 
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <SiTypescript />
+                        <p>Typescript</p>
+                    </div>
 
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <SiPostgresql />
-                <p>Postgresql</p>
-            </div>
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <SiPostgresql />
+                        <p>Postgresql</p>
+                    </div>
 
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <SiRecoil />
-                <p>Recoil</p>
-            </div>
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <SiRedis />
+                        <p>Redis</p>
+                    </div>
 
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <SiMongodb />
+                        <p>MongoDB</p>
+                    </div>
 
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <FaFigma />
-                <p>Figma</p>
-            </div>
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <FaFigma />
+                        <p>Figma</p>
+                    </div>
 
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <SiExpress />
-                <p>Express</p>
-            </div>
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <SiExpress />
+                        <p>Express</p>
+                    </div>
 
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <FaAws />
-                <p>AWS</p>
-            </div>
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <FaCloudflare />
-                <p>Cloudflare</p>
-            </div>
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <FaAws />
+                        <p>AWS</p>
+                    </div>
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <FaCloudflare />
+                        <p>Cloudflare</p>
+                    </div>
 
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <FaDocker />
-                <p>Docker</p>
-            </div>
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <FaDocker />
+                        <p>Docker</p>
+                    </div>
 
-            <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
-                <RiTailwindCssFill />
-                <p>Tailwind</p>
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <RiTailwindCssFill />
+                        <p>Tailwind</p>
+                    </div>
+
+                    <div className='ml-2 bg-neutral-950 px-2 py-1 rounded-xl flex items-center gap-x-2 font-bold text-xl  '>
+                        <p>Framer Motion</p>
+                    </div>
+                </Marquee>
             </div>
-        </Marquee>
-      </div>
-    </div>
-  )
+        </div>
+    )
 }
--- lib/constants.ts
+++ lib/constants.ts
@@ -19,11 +19,6 @@ export const fadeInAnimationVarients = {
     })
 }
 
-
-  
-
-
-
 export const Frontend_skill = [
     {
         skill_name: "React",
@@ -126,14 +121,39 @@ export const ProjectsList = [
     //     hostLink: 'https://moodlog.ashishtiwari.net',
     //     githubLink: 'https://github.com/iamasistiwari/vice-website'
     // },
+
+    {
+        title: "Trufake- deepfake detector",
+        description: "detects whether the user uploaded video is deepfake or not.",
+        icon: ['/tf1.png', '/tf2.png'],
+        detailedDescription: "app lets you detects whether the uploaded video is deepfake or not using model bulit from scratch build on top of pretrained model of huggingFace an request is sent to the model with videoUrl using FastApi and model downloads the video runs the model and send the response to the user.",
+        photos: ['/tf3.png'],
+        techStacks: [ "Turborepo", "CI-CD pipeline", "Framer Motion", 'TypeScript', 'Nextjs-15', "Next Auth" ,'Tailwind', "Postgres", 'Python', "FastApi", "Model build from scratch", ],
+        hosted: 'VPS',
+        hostLink: 'https://trufake.ashishtiwari.net/',
+        githubLink: 'https://github.com/iamasistiwari/trufake'
+    },
+
+    {
+        title: "Playfi– Real-Time Collaborative Music Player",
+        description: "realtime music room app where audience decide to play music..",
+        icon: ['/pf1.png', '/pf2.png'],
+        detailedDescription: "This app allows you to create a room where your audience can join, add songs to a playlist, and vote on them. The top-voted song plays automatically, while only the admin has control over manually changing or playing songs.",
+        photos: ['/pf3.png'],
+        techStacks: [ "Turborepo", "CI-CD pipeline", "Framer Motion", 'TypeScript', 'Nextjs-15', "Next Auth" ,'Tailwind',"Postgres-Main Db","Redis-cache" ,'WebSocket', "Youtube-APIs"],
+        hosted: 'VPS',
+        hostLink: 'https://playfi.ashishtiwari.net/',
+        githubLink: 'https://github.com/iamasistiwari/play-fi'
+    },
+
     {
         title: "Synapse- second brain",
         description: "app lets you stores content ( url, notes, etc ) and allow ai-search through your content.",
         icon: ['/synapse.png'],
         detailedDescription: "This project is a full-stack application built using Turborepo that allows users to create and manage notes. It features a novel editor to add notes, auto-fetches metadata from links, stores content in Supabase PostgreSQL, and supports embeddings search through the content.",
         photos: ['/sy1.png'],
-        techStacks: ['Turborepo', 'TypeScript', 'Nextjs-15', 'Tailwind', 'Supabase-Postgres', 'Express'],
-        hosted: 'Vercel',
+        techStacks: ['Turborepo', 'TypeScript', 'Nextjs-15', 'Tailwind', "Next Auth",'Supabase-Postgres', 'Express'],
+        hosted: 'VPS',
         hostLink: 'https://synapse.ashishtiwari.net',
         githubLink: 'https://github.com/iamasistiwari/synapse-second-brain'
     },
@@ -144,7 +164,7 @@ export const ProjectsList = [
         detailedDescription: "realtime advanced queue based tictactoe game build with nextjs and websocket and all the game logic runs on the websocket server.",
         photos: ['/tc3.png'],
         techStacks: ['TypeScript', 'Nextjs-15', 'Tailwind', 'Websocket'],
-        hosted: 'Vercel',
+        hosted: 'VPS',
         hostLink: 'https://tictactoe.ashishtiwari.net',
         githubLink: 'https://github.com/iamasistiwari/advance-tictactoe'
     },
@@ -154,7 +174,7 @@ export const ProjectsList = [
         icon: ['/moodlog-website.png'],
         detailedDescription: "Mood-Log is a web app that allows users to track their daily moods throughout the year. Built with Next.js and TypeScript, it features secure authentication via NextAuth and uses PostgreSQL for data storage. Users can log their mood each day, view entries on an interactive calendar, and analyze mood patterns over time. The app provides a simple yet effective way to monitor emotional well-being, helping users gain insights into their mental health.",
         photos: ['/moodlog-website-1.png'],
-        techStacks: ['TypeScript', 'Nextjs', 'Tailwind', 'Postgres'],
+        techStacks: ['TypeScript', 'Nextjs', 'Tailwind',"Next Auth" ,'Postgres'],
         hosted: 'Vercel',
         hostLink: 'https://moodlog.ashishtiwari.net',
         githubLink: 'https://github.com/iamasistiwari/mood-log'
